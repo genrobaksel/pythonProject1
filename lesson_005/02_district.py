56973a701c37bdd7d8af152571d2ba4269bc4c36
ehmatthes <eric@example.com>
2022-07-12 19:36:41 +0300

    lesson 5 3_2

--- lesson_005/02_district.py
+++ lesson_005/02_district.py
@@ -14,15 +14,29 @@ from lesson_005.district.soviet_street.house1 import  room2 as rs12
 from lesson_005.district.soviet_street.house2 import  room1 as rs21
 from lesson_005.district.soviet_street.house2 import  room2 as rs22
 from lesson_005 import district as distr
+# def district(*args):
+#     print('На районе живут ...')
+#     print()
+#     for i in args:
+#         if i==[]:
+#             continue
+#         a = ','.join(i)
+#         print(a)
+
 def district(*args):
-    print('На районе живут ...')
-    print()
-    for i in args:
-        if i==[]:
-            continue
-        a = ','.join(i)
-        print(a)
+     print('На районе живут ...')
+     print()
+     a=[]
+     for i in args:
+         if i ==[]:
+             continue
+         for j in i:
+             a.append(j)
+
+         z=','.join(a)
+     print(z)
 district(rc11.folks, rc12.folks, rc21.folks, rc22.folks, rs11.folks, rs12.folks, rs21.folks, rs22.folks)
 
-
+# a=district
+# print(a())
 
